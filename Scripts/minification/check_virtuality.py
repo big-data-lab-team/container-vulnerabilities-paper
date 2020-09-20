cf6696004088bd7590724934360a9eea24344745
mathdugre <mathdugre@pm.me>
2020-09-20 17:51:36 -0400

    [REFACTOR] make nomenclature more consistent + removed redundant code.

--- Scripts/minification/check_virtuality.py
+++ Scripts/minification/check_virtuality.py
@@ -1,32 +1,23 @@
 #!/usr/bin/env python
-
 import sys
-import csv
 
-packages = None
-all = None
-virts = None
 
-f1 = sys.argv[1]
-f2 = sys.argv[2]
-f3 = sys.argv[3]
-with open(f1, "r") as f:
-    packages = f.readlines()
+# Read list of packages from file.
+with open(sys.argv[1], "r") as fin:
+    packages = fin.readlines()
 
-with open("all-packages.csv", "r") as f:
-    all = f.readlines()
+with open("all-packages.csv", "r") as fin:
+    installed = [package.strip() for package in packages if package in fin.readlines()]
 
-with open("virtual.csv", "r") as f:
-    virts = f.readlines()
+with open("virtual.csv", "r") as fin:
+    virtual = [package.strip() for package in packages if package in fin.readlines()]
 
-installed = [p.strip() for p in packages if p in all]
-virtual = [p.strip() for p in packages if p in virts]
-out = open(f2, "w")
-for line in installed:
-    out.write(line)
-    out.write("\n")
-out.close()
-out = open(f3, "w")
-for line in virtual:
-    out.write(line)
-    out.write("\n")
+# Write the installed packages to file.
+with open(sys.argv[2], "w") as fout:
+    for package in installed:
+        fout.write(package +'\n')
+
+# TODO add comment about what is written to the file here.
+with open(sys.argv[3], "w") as fout:
+    for package in virtual:
+        fout.write(package +'\n')
