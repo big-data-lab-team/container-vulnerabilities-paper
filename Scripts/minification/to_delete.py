a736b9a136469bfcec1e0f203e9faa4d19a38713
mathdugre <mathdugre@pm.me>
2020-09-20 17:26:39 -0400

    [REFACTOR] Apply black formatter.

--- Scripts/minification/to_delete.py
+++ Scripts/minification/to_delete.py
@@ -4,18 +4,18 @@
 all = None
 keep = None
 
-with open('all-packages.csv', 'r') as f:
-  all = f.readlines()
+with open("all-packages.csv", "r") as f:
+    all = f.readlines()
 
-with open('uniq_keep_installed.csv', 'r') as f:
-  keep = f.readlines()
+with open("uniq_keep_installed.csv", "r") as f:
+    keep = f.readlines()
 
-remove = [ p.strip() for p in all if not p in keep ]
-str3 = ' '.join(str(e) for e in remove)
+remove = [p.strip() for p in all if not p in keep]
+str3 = " ".join(str(e) for e in remove)
 
-with open('to_remove.txt', 'w') as f:
-      f.write(str3)
+with open("to_remove.txt", "w") as f:
+    f.write(str3)
 print(str3)
 print(len(remove))
-#for p in remove:
+# for p in remove:
 #    print(p)
