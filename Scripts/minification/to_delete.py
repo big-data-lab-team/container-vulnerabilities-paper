bfd43e4579731f177899598f65df12ec2e7b0a0e
mathdugre <mathdugre@pm.me>
2020-09-22 12:41:04 -0400

    FIXUP

--- Scripts/minification/to_delete.py
+++ Scripts/minification/to_delete.py
@@ -6,7 +6,7 @@ with open("uniq_keep_installed.csv", "r") as fin:
     keep = fin.readlines()
 
 remove = [package.strip() for package in packages if package not in keep]
-removed_packages: str = " ".join(str(e) for e in remove)
+removed_packages = " ".join(str(e) for e in remove)
 
 with open("to_remove.txt", "w") as f:
     f.write(removed_packages)
