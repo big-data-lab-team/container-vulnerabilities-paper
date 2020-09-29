72ac92f5f5d2d5df2a739ab6e6267bcd7ba7115b
Bhupinder Kaur <bhangu.bhupinder@gmai.com>
2020-09-28 23:17:23 -0400

    added centos mapping

--- Scripts/compare_results/AnchoremapRHSAtoCVE.py
+++ Scripts/compare_results/AnchoremapRHSAtoCVE.py
@@ -4,13 +4,25 @@ import csv
 import sys
 content = []
 result = []
-cves = []
+rhsa = {}
+cves={}
 def domapping(file1,file2):
     with open(file1,'r') as fp:
-              rows=fp.readlines()
+               rows = [line.split() for line in fp]
+            #  rows=fp.readlines()
+    del rows[0]
     for row in rows:
-        row = row.strip()
-    
+        vul = row[0]
+        severity = row[2]
+        package = row[1]
+        print(vul)
+        print(severity)
+        print(package)
+        rhsa[vul]= [package,severity]
+        #rhsa[vul][1] = severity
+        #row = row.strip()
+    print(rhsa)
+    print(len(rhsa))
     #Read the XML file
     with open("redhat.xml", "r") as file:
         # Read each line in the file, readlines() returns a list of lines
@@ -18,23 +30,33 @@ def domapping(file1,file2):
         # Combine the lines in the list into a string
         content = "".join(content)
         bs_content = bs(content, "lxml")
-        for row in rows:
-         row = row.strip()
-         req_id = bs_content.find("reference", {"ref_id": row})
+        for key, value in rhsa.items():
+         vul = key
+         severity = value[1]
+         package = value[0]
+         req_id = bs_content.find("reference", {"ref_id": vul})
          result = list(req_id.next_siblings)
+         #print(result)
          for each in result:
            try:
-               cves.append(each['ref_id'])
+               
+               cves[each['ref_id']]=[package,severity]
+               #cves[each['ref_id']][1]=severity
                print(each['ref_id'])
            except:
              continue
-    cves_req = list(set(cves))
+    print(cves)
+    print(len(cves))
     #print(cves_req)
     #print(len(cves_req))
     with open(file2,'w') as out:
-            for entry in cves_req:
-                out.write(entry)
-                out.write("\n")
+        out.write("Vulnerability_ID   Package   Severity\n")
+        for key in cves.keys():
+          st = key+"   "+cves[key][0]+"   "+cves[key][1]
+          out.write("%s\n"%(st))
+   #         for item in cves:
+   #             out.write()
+   #             out.write("\n")
 
 def main():
     file1=sys.argv[1] ## csv file containing RHSA and needed to be mapped
--- Scripts/compare_results/ClairmapRHSAtoCVE.py
+++ Scripts/compare_results/ClairmapRHSAtoCVE.py
@@ -2,31 +2,34 @@ import sys
 import csv
 
 targets =[]
-cves = []
+cves = {}
+rhsa={}
+severity=''
 def domapping(file1,file2,file3):
   with open(file1,'r') as f1, open(file2,'r') as f2:
-      words = f2.readlines()
+      words = [line.split() for line in f2]
       lines=f1.readlines()
       for word in words:
-        word=word.strip()
-        print(word)
-        #targets.append([line for line in lines if word in line])
+          if(len(word) >1):
+              if (word[3] != "|" and word[4] != "|"):
+                        rhsa[word[4]]=[word[3],word[6]]
+  for key in rhsa.keys():
+        severity = rhsa[key][0]
+        package = rhsa[key][1]
+        print("severity  ",severity)
+        print("package ",package) 
         for line in lines:
-            if word in line:
+            if key in line:
                 data = line.split() 
-                targets.append(data[1])
-      for target in targets:
-        #print(target)
-        target = target.split(',')
-        for t in target:
-            cves.append(t)
-  req_cves = list(set(cves))
-  with open(file3,'w') as f:
-      for cve in req_cves:
-          f.write(cve)
-          f.write("\n")
-          print(cve)
-
+                for cve in data[1].split(','):
+                    cves[cve]=[severity,package]
+  print(cves)
+  print(len(cves))
+  with open(file3,'w') as out:
+        out.write("Vulnerability_ID     Package    Severity\n")
+        for key in cves.keys():
+            st = key+"   "+cves[key][1]+"   "+cves[key][0]
+            out.write("%s\n"%(st))
 def main():
     file1=sys.argv[1]   ## downloaded RHSA to CVE mapping file (rhsamapcpe.txt file in our case)
     file2=sys.argv[2]   ## csv file containing RHSA to convert
